497896ce50532a85306930fafeb0b5ac561d03f7
Yoann Fleury <yoann.fleury@yahoo.com>
2021-01-13 17:27:54 +0100

    feat: running fetch headlessa and sandbox arg

--- src/commands/vaccins.js
+++ src/commands/vaccins.js
@@ -12,7 +12,10 @@ module.exports = {
       "⬇️ Récupération des données en cours, veuillez patienter"
     );
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
     const page = await browser.newPage();
     await page.goto("https://covidtracker.fr/vaccintracker/", {
       waitUntil: "networkidle2",
